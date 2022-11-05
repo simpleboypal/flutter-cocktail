e5720858b691d4f5b2927a8473b6af77aa263a3b
Pallav <simpleboypallav@gmail.com>
2022-11-05 16:11:16 +0545

    first

---
+++ lib/drink_detail.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class DrinkDetail extends StatelessWidget {
+  final drink;
+
+  const DrinkDetail({@required this.drink});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        myColor,
+        Colors.orange,
+      ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(drink["strDrink"]),
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: drink["idDrink"],
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(
+                    drink["strDrinkThumb"],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "ID: ${drink["idDrink"]}",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "${drink["strDrink"]}",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:cocktail/homepage.dart';
+import 'package:flutter/material.dart';
+
+const Color myColor = Colors.brown;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Cocktail App",
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Homepage(),
+    );
+  }
+}
